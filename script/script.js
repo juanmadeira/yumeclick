48d7401d8293ecb96cef2248d27e0304adb4f827
Juan <juanpmad@outlook.com>
2023-06-17 19:23:16 -0300

    script

--- script/script.js
+++ script/script.js
@@ -1,6 +1,25 @@
-let score = 0;
+/* querySelector const */
+const qs = (val) => {
+    return document.querySelector(val)
+}
 
-function addToScore(amount) {
-    score = score + amount;
-    document.getElementById("score").innerHTML = score;
+let score = 0;
+let click = 0;
+
+function updateScore(click) {
+    score += click;
+    qs("#score").innerHTML = score;
+    qs("title").innerHTML = score + " sonhos | yumeclick"
+}
+
+function storageScore() {
+    const sonhos = localStorage.getItem("score") || 0;
+    const efeitos = JSON.parse(localStorage.getItem("efeitos")) || [];
+
+    const storage = {
+        "sonhos": sonhos,
+        "efeitos": efeitos
+    }
+
+    return storage;
 }
