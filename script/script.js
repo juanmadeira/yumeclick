dde4cebd6dfefc76010f0e7f052d364a6c27ee92
Juan <juanpmad@outlook.com>
2023-06-17 22:31:57 -0300

    script

--- script/script.js
+++ script/script.js
@@ -7,16 +7,23 @@ const qs = (val) => {
 let score = 0;
 let cps = 0;
 
+/* sons */
+const buySound = new Audio("./sound/buy-sound.wav");
+const errorSound = new Audio("./sound/error-sound.wav");
+buySound.volume = 0.2;
+errorSound.volume = 0.2;
+
 /* eventos */
 qs("#buy-frog").addEventListener("click", function() {
     if (score >= 5) {
         score -= 5;
         cps += 1;
+        buySound.play();
         qs("#score").innerHTML = score;
         qs("title").innerHTML = score + " sonhos | yumeclick";
     }
     else {
-        
+        errorSound.play();
     }
 });
 
